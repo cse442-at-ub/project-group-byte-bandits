19d84a357b515d132dea5b297d0d49a0c64cabdb
joechen43 <jchen526@buffalo.edu>
2023-09-11 09:43:45 -0400

    setting up navigation

---
+++ src/screens/Login.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const Login = ({navigation}) => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
