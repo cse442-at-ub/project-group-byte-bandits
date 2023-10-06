ec97b5bf21aecc49aea76f8d7ab37c4e45f94d89
Joe Alessi <jjalessi@buffalo.edu>
2023-10-06 16:43:00 -0400

    added Bubble Logo to the login screen

--- app/src/screens/Login.js
+++ app/src/screens/Login.js
@@ -1,18 +1,18 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+// import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+// import React from 'react'
 
-const Login = ({navigation}) => {
-  return (
-    <View style={{
-        marginTop:50,
-        justifyContent:'center',
-        alignItems:'center',
-      }}>
-        <Text style={{fontSize:20}}>Login</Text>
-    </View>
-  )
-}
+// const Login = ({navigation}) => {
+//   return (
+//     <View style={{
+//         marginTop:50,
+//         justifyContent:'center',
+//         alignItems:'center',
+//       }}>
+//         <Text style={{fontSize:20}}>Login</Text>
+//     </View>
+//   )
+// }
 
-export default Login
+// export default Login
 
-const styles = StyleSheet.create({})
+// const styles = StyleSheet.create({})
