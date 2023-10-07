72f914c6b5e893332a016cf0ecbdd279cb8c04fc
rsenic <63600284+VX59@users.noreply.github.com>
2023-10-07 19:48:38 -0400

    Update logout.php

--- app/src/auth/logout.php
+++ app/src/auth/logout.php
@@ -3,20 +3,25 @@ include "connect.php";
 
 try {
     $time_seconds = 10;
-    $session_id = $_COOKIE['session'];
-    $result = $connection->query("SELECT * FROM `user_data` WHERE `session` = '$session_id'");
-    $row = $result->fetch_assoc();
-    if($row) {
-        if(!$connection->query("UPDATE `user_data` SET `session` = '' WHERE `session` = '$session_id'")) {
-            throw new Exception('failed to remove session id from database');
+    if($_COOKIE['session']) {
+        $session_id = $_COOKIE['session'];
+        $result = $connection->query("SELECT * FROM `user_data` WHERE `session` = '$session_id'");
+        $row = $result->fetch_assoc();
+        if($row) {
+            if(!$connection->query("UPDATE `user_data` SET `session` = '' WHERE `session` = '$session_id'")) {
+                throw new Exception('failed to remove session id from database');
+            }
+            unset($_COOKIE['session']);
+            session_id($session_id);
+            if(!setcookie("session", "",  time() - $time_seconds, "cse.buffalo.edu/~jderosa3/")) {
+                throw new Exception('failed to remove session id from cookies');
+            }
+            session_destroy();
         }
-        unset($_COOKIE['session']);
-        session_id($session_id);
-        if(!setcookie("session", "",  time() - $time_seconds, "cse.buffalo.edu/~jderosa3/")) {
-            throw new Exception('failed to remove session id from cookies');
-        }
-        session_destroy();
+    } else {
+        throw new Exception('there is no logged in user');
     }
+    
 } catch(Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
     exit(99);
