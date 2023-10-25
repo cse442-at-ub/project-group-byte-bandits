f5c7a87f759ec558c422988cc2f5255d1f1b042c
Jacob DeRosa <jderosa3@buffalo.edu>
2023-10-25 16:00:10 -0400

    adding crud ops and implementing security

--- app/src/auth/validate_login.php
+++ app/src/auth/validate_login.php
@@ -2,38 +2,42 @@
 include "utility.php";
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $session_id = $_COOKIE['PHPSESSID'];
+    if($session_id) {
+        $rows = get_with_sid($session_id,$connection);
+        if (count($rows) > 0) {
+            header("Location: ../chatroom/message_board"); 
+        }
+    }
     check_post_record($_POST);
     
     $uname = $_POST['username'];
     $pword = $_POST['password'];
-    $hashed_pword = hash("sha256", $pword);
+
     try {
-        if(!$row = get_with_name($uname,$connection)) {
-            if(!$row = get_with_email($uname,$connection))
-                throw new Exception('no record found in database');
+        $rows = get_with_name($uname,$connection);
+        if(count($rows) === 0) {
+            $email_hashed = hash("sha256",$uname);
+            $rows = get_with_email($email_hashed,$connection);
+            if(count($rows) === 0)
+               throw new Exception('no record found in database');
         }
-        if(!hash_equals($row['password'], $hashed_pword))
-            throw new Exception('password does not match record');
+        if(!password_verify($pword, $rows[0]['password']))
+            new Exception('password does not match record');
+
+        // rehash password after evaluation ... random salt
+        $pword_rehash = password_hash($pword, PASSWORD_BCRYPT);
+        reset_password($rows[0]['password'],$pword_rehash,$session_id, $connection);
 
     } catch( Exception $e) {
         echo 'Caught exception: ',  $e->getMessage(), "\n";
         exit($errc['form']);
     }
 
-    $time_seconds = 3600;
-    $session_id = session_create_id('auth-');
-    session_id($session_id);
-
-    try {
-        $connection->query("UPDATE `user_data` SET `session` = '$session_id' WHERE `name` = '$uname'");
-        if($connection->connect_error) 
-            throw new Exception("connection failed: " . $connection->connect_error);
-        
-    } catch(Exception $e) {
-        echo 'Caught exception: ',  $e->getMessage(), "\n";
-        exit($errc['sql']);
-    }
-
+    $new_session_id = session_create_id('auth-');
+    session_id($new_session_id);
+    reset_session_id($new_session_id, $uname, $connection);
     session_start();
-    header("Location: ../send_messages/send_xhr");
+} else {
+    echo "<h1>access not permitted!! (╯°□°）╯︵ ┻━┻</h1>";
 }
