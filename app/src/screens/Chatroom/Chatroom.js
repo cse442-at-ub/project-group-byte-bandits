b1c2846ef27facc12a55b29cabf1a501da9ff75a
Joe Alessi <jjalessi@buffalo.edu>
2023-11-08 00:00:32 -0500

    added KeyboardAvoidingView for Chatroom

--- app/src/screens/Chatroom/Chatroom.js
+++ app/src/screens/Chatroom/Chatroom.js
@@ -6,6 +6,9 @@ import {
   Text,
   TouchableOpacity,
   TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
 } from "react-native";
 import {
   Entypo,
@@ -16,153 +19,128 @@ import {
 
 export const Chatroom = () => {
   return (
-    <View style={styles.ChatroomBackground}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.topBar}>
-          <TouchableOpacity style={styles.leaveButton}>
-            <Entypo name="chevron-left" size={32} color="red" />
-            <Text style={styles.leaveText}>Leave</Text>
-          </TouchableOpacity>
-          {/* padding View */}
-          <View
-            style={{ display: "flex", height: "100%", width: "70%" }}
-          ></View>
-          <View style={styles.leaveButton}>
-            <TouchableOpacity>
-              <FontAwesome5 name="user-friends" size={32} color="#555454" />
-            </TouchableOpacity>
-          </View>
-        </View>
+    <SafeAreaView style={styles.ChatroomBackground}>
+      <View style={styles.topBar}>
+        <TouchableOpacity style={styles.leaveButton}>
+          <Entypo name="chevron-left" size={32} color="red" />
+          <Text style={styles.leaveText}>Leave</Text>
+        </TouchableOpacity>
+        <View style={{ flex: 1 }} />
+        <TouchableOpacity style={styles.leaveButton}>
+          <FontAwesome5 name="user-friends" size={32} color="#555454" />
+        </TouchableOpacity>
+      </View>
 
-        <View style={styles.chatroomTitle}>
-          <Text style={styles.chatroomTitleText}>Machine Learning Bubble</Text>
-        </View>
+      <View style={styles.chatroomTitle}>
+        <Text style={styles.chatroomTitleText}>Machine Learning Bubble</Text>
+      </View>
 
-        {/* WHERE CHATS WILL RENDER */}
-        <View style={styles.chatroomBody}></View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.keyboardAvoid}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 0}
+      >
+        <ScrollView style={styles.chatroomBody}>
+          {/* Chat messages will go here */}
+        </ScrollView>
+
         <View style={styles.bottomBar}>
-          <View style={styles.mapIcon}>
-            <TouchableOpacity>
-              <MaterialCommunityIcons
-                name="map-search-outline"
-                size={50}
-                color={"#56585B"}
-              />
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity style={styles.mapIcon}>
+            <MaterialCommunityIcons
+              name="map-search-outline"
+              size={50}
+              color={"#56585B"}
+            />
+          </TouchableOpacity>
           <View style={styles.textBox}>
             <TextInput
               style={styles.searchBar}
-              placeholder=""
+              placeholder="Type a message..."
               placeholderTextColor={"#3D3C3C"}
-              maxLength={10}
+              maxLength={40}
             />
           </View>
-          <View style={styles.sendMessage}>
-            <TouchableOpacity>
-              <Feather
-                name="send"
-                size={40}
-                color={"#555454"}
-                style={{ transform: [{ rotate: "45deg" }] }}
-              />
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity style={styles.sendMessage}>
+            <Feather
+              name="send"
+              size={42}
+              color={"#555454"}
+              style={{ transform: [{ rotate: "45deg" }] }}
+            />
+          </TouchableOpacity>
         </View>
-      </SafeAreaView>
-    </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  searchBar: {
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-    height: "55%",
-    width: "100%%",
-    borderRadius: 30,
-    borderColor: "#3D3C3C",
-    borderWidth: 1,
-    backgroundColor: "#3C3B3B",
-    color: "#56585B",
-    fontWeight: "bold",
-    paddingLeft: 15,
-    paddingRight: 85,
-  },
-  sendMessage: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "flex-start",
-    height: "60%",
-    width: "15%",
-  },
-  textBox: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "60%",
-    width: "70%",
-  },
-  mapIcon: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "60%",
-    width: "15%",
-  },
-  bottomBar: {
-    display: "flex",
-    flexDirection: "row",
-    height: "20%",
-    width: "100%",
-  },
-  chatroomBody: {
-    display: "flex",
-    height: "70%",
-    width: "100%",
-  },
-  chatroomTitleText: {
-    fontWeight: "bold",
-    fontSize: 32,
-    color: "#93B8DA",
-  },
-  chatroomTitle: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "10%",
-    width: "100%",
-  },
-  leaveText: {
-    fontWeight: "bold",
-    fontSize: 16,
-    color: "red",
-  },
-  leaveButton: {
-    display: "flex",
-    height: "100%",
-    width: "15%",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "center",
+  ChatroomBackground: {
+    flex: 1,
+    backgroundColor: "#191818",
   },
   topBar: {
     display: "flex",
     flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
     height: "5%",
-    width: "100%",
+    backgroundColor: "#191818",
+  },
+  leaveButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  chatroomTitle: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "10%",
+    backgroundColor: "#191818",
+  },
+  chatroomTitleText: {
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "#93B8DA",
+  },
+  keyboardAvoid: {
+    flex: 1,
+  },
+  chatroomBody: {
+    flex: 1,
+    backgroundColor: "#232222",
   },
 
-  ChatroomBackground: {
+  bottomBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  mapIcon: {
     flex: 1,
-    height: "100%",
-    width: "100%",
-    backgroundColor: "#191818",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textBox: {
+    flex: 5,
+    justifyContent: "center",
+  },
+  sendMessage: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  searchBar: {
+    padding: 10,
+    height: 50,
+    borderRadius: 30,
+    backgroundColor: "#3C3B3B",
+    color: "#56585B",
+    fontWeight: "bold",
+  },
+  leaveText: {
+    fontSize: 16,
+    color: "red",
+    marginLeft: 5,
   },
 });
